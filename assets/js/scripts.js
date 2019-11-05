3e290c334eb033a320d28a6b1b4f3a1cab1926d7
Oguz Bilgin <oguz.bilginnn@gmail.com>
2019-11-05 16:53:26 +0300

    added preview,detail,img-slider,contact sections and made some improvements

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,17 +1,84 @@
 var mySwiper = new Swiper('.swiper-container', {
 
     direction: 'horizontal',
-    loop: true,
+    loop:true,
 
-    slidesPerView: 3,
+    slidesPerView: 1,
     spaceBetween: 30,
+
+    breakpoints: {
+        480:{
+            slidesPerView:3,
+            spaceBetween:30,
+        }},
+    
+
+
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
     },
+
+    autoplay: {
+        delay: 4000,
+      },
 
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     }
 })
+
+
+$('.popup-with-move-anim').magnificPopup({
+    type: 'inline',
+
+    fixedContentPos: false,
+    fixedBgPos: true,
+
+    overflowY: 'auto',
+
+    closeBtnInside: true,
+    preloader: false,
+    
+    midClick: true,
+    removalDelay: 300,
+    mainClass: 'my-mfp-slide-bottom'
+});
+
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
